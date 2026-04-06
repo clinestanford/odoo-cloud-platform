17d55d4cd2f2cbe5ebd8379d9b1464d20bdd0795
Stanford Cline <clinestanford@gmail.com>
2026-04-06 13:43:56 -0600

    want fs_document to be on 17.0

---
+++ fs_document/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Documents Filesystem Storage Bridge",
+    "summary": "Enables Documents app to serve files stored in external filesystem storage (S3, etc.)",
+    "version": "17.0.1.0.0",
+    "author": "JobXcel, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "category": "Knowledge Management",
+    "depends": ["documents", "fs_attachment"],
+    "website": "https://github.com/JobXcel-AI",
+    "installable": True,
+    "auto_install": True,
+}
