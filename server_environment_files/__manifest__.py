0dd88df01d0d063e73f25d37a983caac39ae3163
Stanford Cline <clinestanford@gmail.com>
2026-04-06 14:01:29 -0600

    want a base for all the files

---
+++ server_environment_files/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Example server configuration environment files repository module",
+    "version": "17.0.0",
+    "depends": ["server_environment"],
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "summary": "sample config file for server_environment",
+    "website": "https://github.com/OCA/server-env",
+    "license": "GPL-3 or any later version",
+    "category": "Tools",
+    "data": [],
+}
